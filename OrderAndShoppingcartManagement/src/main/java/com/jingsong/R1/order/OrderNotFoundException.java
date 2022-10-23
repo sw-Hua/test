77c4ee1c56b5d83fe4866fe13399a59b931dd106
sw-Hua <59052152+sw-Hua@users.noreply.github.com>
2022-10-23 19:45:49 +1100

    Delete OrderAndShoppingcartManagement directory

--- OrderAndShoppingcartManagement/src/main/java/com/jingsong/R1/order/OrderNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.jingsong.R1.order;
-
-public class OrderNotFoundException extends Throwable {
-    public OrderNotFoundException(String message) {
-        super(message);
-    }
-}
--- OrderAndShoppingcartManagement/src/main/java/com/jingsong/R1/order/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jingsong.R1.order;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<Order, Integer> {
-
-    //public Long countByID(Integer id);
-}
